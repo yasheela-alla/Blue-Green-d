1ff5a204af4b531d049c723296c46515a7824b2c
Ay4sh <yasheela435@gmail.com>
2024-09-28 19:40:29 +0530

    Update variables.tf

--- Cluster/variables.tf
+++ Cluster/variables.tf
@@ -1,6 +1,6 @@
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
+  description = "The name of the SSH key pair to use for vm"
   type        = string
-  default     = "DevOps-Shack"
+  default     = "Server_Key"
 }
 
