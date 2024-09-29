9b180ad322e7b742ed1cc184afc9d4a22b740288
Ay4sh <yasheela435@gmail.com>
2024-09-29 11:55:17 +0530

    Delete Cluster/output.tf

--- Cluster/output.tf
+++
@@ -1,9 +0,0 @@
-output "aks_cluster_name" {
-  description = "Name of the AKS Cluster"
-  value       = azurerm_kubernetes_cluster.y_aks.name
-}
-
-output "aks_node_resource_group" {
-  description = "Resource group for the AKS nodes"
-  value       = azurerm_kubernetes_cluster.y_aks.node_resource_group
-}
