edb08708157f3bbc093f65fc36409e34eca76d54
Ay4sh <yasheela435@gmail.com>
2024-09-29 07:49:38 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -1,119 +1,90 @@
-provider "azurerm" {
-  features {}
+# Resource Group
+resource "azurerm_resource_group" "yasheela_rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "r-grp"
-  location = "East Japan"
-}
-
-resource "azurerm_virtual_network" "vnet" {
+# Virtual Network
+resource "azurerm_virtual_network" "yasheela_vnet" {
   name                = "yasheela-vnet"
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-
-  tags = {
-    Name = "yasheela-vnet"
-  }
+  address_space       = [var.vnet_cidr]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.yasheela_rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  count               = 2
-  name                = "yasheela-subnet-${count.index}"
-  resource_group_name = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes    = ["10.0.${count.index}.0/24"]
-
-  tags = {
-    Name = "yasheela-subnet-${count.index}"
-  }
+# Subnets
+resource "azurerm_subnet" "yasheela_subnet" {
+  count                 = 2
+  name                  = "yasheela-subnet-${count.index}"
+  resource_group_name   = azurerm_resource_group.yasheela_rg.name
+  virtual_network_name  = azurerm_virtual_network.yasheela_vnet.name
+  address_prefixes      = [element(var.subnet_prefix, count.index)]
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "yasheela-public-ip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-
-  tags = {
-    Name = "yasheela-public-ip"
-  }
-}
-
-resource "azurerm_network_security_group" "nsg" {
+# Network Security Group for AKS Cluster
+resource "azurerm_network_security_group" "yasheela_nsg" {
   name                = "yasheela-nsg"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
+  resource_group_name = azurerm_resource_group.yasheela_rg.name
 
-  tags = {
-    Name = "yasheela-nsg"
+  security_rule {
+    name                       = "AllowAllOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_network_security_rule" "allow_ssh" {
-  name                        = "Allow-SSH"
-  priority                    = 100
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "yasheela_aks" {
+  name                = "yasheela-aks"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.yasheela_rg.name
+  dns_prefix          = "yasheelaaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.yasheela_subnet[0].id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "calico"
+  }
+
+  depends_on = [azurerm_subnet.yasheela_subnet]
+}
+
+# SSH Key for Node Access
+resource "azurerm_ssh_public_key" "yasheela_ssh_key" {
+  name                = "yasheela-ssh-key"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.yasheela_rg.name
+  public_key          = file(var.ssh_key_name)
+}
+
+# Node Security Group for SSH Access
+resource "azurerm_network_security_rule" "yasheela_ssh" {
+  name                        = "SSHAllowRule"
+  priority                    = 101
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "*"
+  source_address_prefix        = "*"
   destination_address_prefix  = "*"
-  network_security_group_name = azurerm_network_security_group.nsg.name
-  resource_group_name         = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "yasheela-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "yasheela"
-
-  agent_pool_profile {
-    name       = "agentpool"
-    count      = 3
-    vm_size    = "Standard_DS2_v2"
-    os_type    = "Linux"
-    max_pods   = 110
-    mode       = "System"
-    availability_zone = ["1", "2", "3"]
-  }
-
-  linux_profile {
-    admin_username = "azureuser"
-    ssh_key {
-      key_data = var.ssh_key
-    }
-  }
-
-  service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
-  }
-
-  role_based_access_control {
-    enabled = true
-  }
-
-  network_profile {
-    network_plugin = "azure"
-    load_balancer_sku = "standard"
-  }
-
-  tags = {
-    Name = "yasheela-aks"
-  }
-}
-
-variable "ssh_key" {
-  description = "SSH public key for the AKS cluster"
-}
-
-variable "client_id" {
-  description = "Client ID of the Service Principal"
-}
-
-variable "client_secret" {
-  description = "Client Secret of the Service Principal"
+  resource_group_name         = azurerm_resource_group.yasheela_rg.name
+  network_security_group_name = azurerm_network_security_group.yasheela_nsg.name
 }
