0b0ad1f1351b3abaf94fdca098adc3b9d8d9bb22
Ay4sh <yasheela435@gmail.com>
2024-09-29 08:04:37 +0530

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,9 +1,9 @@
 output "aks_cluster_name" {
   description = "Name of the AKS Cluster"
-  value       = azurerm_kubernetes_cluster.yasheela_aks.name
+  value       = azurerm_kubernetes_cluster.y_aks.name
 }
 
 output "aks_node_resource_group" {
   description = "Resource group for the AKS nodes"
-  value       = azurerm_kubernetes_cluster.yasheela_aks.node_resource_group
+  value       = azurerm_kubernetes_cluster.y_aks.node_resource_group
 }
